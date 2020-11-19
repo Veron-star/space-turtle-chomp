7d32b0ccd7ebb04b88a5109a3029b0f9f75e83d0
veronica lim <veron_yan@yahoo.com.sg>
2020-11-19 10:24:41 +0800

    setup keyboard for game

--- spaceturtle.py
+++ spaceturtle.py
@@ -1,18 +1,31 @@
-Python 3.8.6 (tags/v3.8.6:db45529, Sep 23 2020, 15:52:53) [MSC v.1927 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> import turtle
->>> turtle.setup(650,650)
->>> wn = turtle.Screen()
->>> wn.bgcolor('navy')
->>> wn.bgcolor('lightcoral')
->>> wn.bgcolor('darkseagreen')
->>> player = turtle.Turtle()
->>> player.color('darkorange')
->>> player.color('midnightblue')
->>> player.shape('turtle')
->>> player.penup()
->>> speed = 1
->>> player.forward(speed)
->>> while True:
+import turtle
+turtle.setup(650,650)
+wn = turtle.Screen()
+wn.bgcolor('navy')
+wn.bgcolor('lightcoral')
+wn.bgcolor('darkseagreen')
+player = turtle.Turtle()
+player.color('darkorange')
+player.color('midnightblue')
+player.shape('turtle')
+player.penup()
+player.speed(0)
+speed = 1
+def turn_left():
+	player.left(30)
+def turn_right():
+	player.right(30)
+def increase_speed():
+	global speed
+	speed += 1
+turtle.listen()
+turtle.onkey(turn_left, 'Left')
+turtle.onkey(turn_right, 'Right')
+turtle.onkey(increase_speed, 'Up')
+player.forward(speed)
+while True:
 	player.forward(speed)
+
+
+
 	
